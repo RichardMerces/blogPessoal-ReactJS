9d56e1b882758c33e05a39a022a220b469ac5676
RichardMerces <richardmerces@outlook.com>
2022-11-24 12:15:51 -0300

    navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './paginas/home/Home';
+import { Grid } from '@material-ui/core';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 
 function App() {
   return (
     <>
-      <Home />
+        <Navbar />
+        <Home />
+        <Footer />
+      {/* <Grid container>
+            <Grid item xs={12} sm={12}>
+                <Home />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Home />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Home />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Home />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Home />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Home />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Home />
+            </Grid>
+      </Grid> */}
     </>
     // Todo componente precisa de um tsx e um css.
+
+    // trocar a versão do MUI para v4 no site da documentação
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,59 @@
-import React from "react";
-import imagem from './img/react.png';
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
 import './Home.css';
 
+function Home() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "Teal" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
+
+{/*
 function Home()  {
     return (
     <>
-        <div className="titulo">
+        <Box display='flex'flexDirection='column' justifyContent='center' alignItems='center' mb='2'>
+            <h1>Home</h1>
+        </Box>
+        <Box display='flex' justifyContent='center'>
+            <Button>Botão 1</Button>
+            <Button>Botão 2</Button>
+        </Box>
+
+          Container Principal
+        <Grid container>
+            <Grid item xs={12} sm={6} style={{background: 'orange', height: '100vh'}}>Item 1</Grid>
+            <Grid item xs={12} sm={6} style={{background: 'lightgrey', height: '100vh'}}>Item 2</Grid>
+            <Grid item xs={12} sm={6} style={{background: 'pink', height: '100vh'}}>Item 3</Grid>
+             Container Interno 
+            <Grid item xs={12} sm={6}>
+                <Grid item direction="column" style={{background: 'orange', height: '50vh'}}>Item 4</Grid>
+                <Grid item direction="column" style={{background: 'lightgrey', height: '50vh'}}>Item 5</Grid>
+            </Grid>
+        </Grid> */}
+        {/* <div className="titulo">
             <h1>Olá Mundo!</h1>
 
             <h2>Aprendendo ReactJs com a Generation Brasil</h2>
@@ -13,9 +61,8 @@ function Home()  {
 
         <div className="container">
             <img src={imagem} className="img" alt="imagem" />
-        </div>
+        </div> 
     </>
     )
 }
-
-export default Home;
+*/}
